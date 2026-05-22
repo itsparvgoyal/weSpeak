c85f5d53b62fcbb884113b4f9b77bd727e9e0430
Parv Goyal <parvgoyal755@gmail.com>
2026-05-22 14:28:13 +0530

    url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://wespeak.onrender.com',
         changeOrigin: true,
         secure: false,
       },
