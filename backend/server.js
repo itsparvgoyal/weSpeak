762af6102ffcfbc7b19b043d2ccbe030666b3d68
Parv Goyal <parvgoyal755@gmail.com>
2026-05-22 15:31:45 +0530

    updated url

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: "https://we-speak-theta.vercel.app",
-  credentials: true,
-}));
-
+// app.use(cors({
+//   origin: "https://we-speak-theta.vercel.app",
+//   credentials: true,
+// }));
+app.use(cors());
 app.use(express.json());
 
 // DB connection
